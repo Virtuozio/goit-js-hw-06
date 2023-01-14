277815c59c9e02906dc412948652ed8bf6887e08
Bogdan Nazarchuk <111483751+Virtuozio@users.noreply.github.com>
2023-01-14 10:12:13 +0200

    Update task-08.js

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,17 +1,15 @@
 const form = document.querySelector("form.login-form");
 
-form.addEventListener("submit", handleSubmit);
-
-function handleSubmit(event) {
+form.addEventListener("submit", (event) => {
   event.preventDefault();
   const {
     elements: { login, password },
   } = event.currentTarget;
-
+  console.log(login.value, password.value);
   if (login.value === "" || password.value === "") {
     return alert("Будь-ласка заповніть усі поля!");
   }
 
   console.log(`Login: ${login.value}, Password: ${password.value}`);
   event.currentTarget.reset();
-}
+});
